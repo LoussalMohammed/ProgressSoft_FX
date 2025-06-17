fc51081c8b96495554bd432b7c42fe3e1910959b
mohammed loussal <hplaptopsimo@gmail.com>
2025-06-17 18:41:17 +0100

    feature: add Api constants for api config prefix

---
+++ src/main/java/org/progresssoft/fxDeals/shared/constant/ApiConstants.java
@@ -0,0 +1,8 @@
+package org.progresssoft.fxDeals.shared.constant;
+
+public final class ApiConstants {
+    public static final String API_VERSION = "v2";
+    public static final String API_BASE_PATH = "/api/" + API_VERSION;
+
+    private ApiConstants() {}
+}
