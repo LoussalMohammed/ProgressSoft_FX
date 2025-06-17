0289b63c992110e6ce865b1f8a7d23960f4f456e
mohammed loussal <hplaptopsimo@gmail.com>
2025-06-17 18:56:27 +0100

    feature: add Duplicate Deal custom excetion

---
+++ src/main/java/org/progresssoft/fxDeals/shared/exception/DuplicateDealIdException.java
@@ -0,0 +1,7 @@
+package org.progresssoft.fxDeals.shared.exception;
+
+public class DuplicateDealIdException extends RuntimeException {
+    public DuplicateDealIdException(String message) {
+        super(message);
+    }
+}
